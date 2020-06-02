e183761602175503a8b047efdc34ea95564256eb
moxxiq <dimanavsisto@gmail.com>
2020-06-02 22:21:29 +0300

    added EDate

--- GigWeb/Models/Event.cs
+++ GigWeb/Models/Event.cs
@@ -1,22 +1,25 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigWeb.Models
 {
     public class Event
     {
-        public Event(string name, string information)
+        public Event(string name, string information = "None")
         {
             Name = name;
+            Information = information;
         }
 
         public string Name { get;set;}
-
         public HashSet<Zone> SitZones { get; set; }
         // List of pathes to images
         public List<string> Images { get; set; }
         public string Information { get; set; }
-
+        [DataType(DataType.Date)]
+        public DateTime EventDate { get; set; }
         public int TicketsLeft()
         {
             return SitZones.Sum(zone => zone.FreeSits);
